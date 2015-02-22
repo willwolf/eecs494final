1a1cb6524d71eee4c2cd344f8d96c512c45dfec7
Billy Wolfington <man_of_magik903@yahoo.com>
2015-02-21 19:59:57 -0500

    Basic player movement script started

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int playerNum  = 0;
+	public float rotateSpeed = 90f;
+	public float walkSpeed = 8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizInput = Input.GetAxis("Horizontal_" + playerNum.ToString()),
+			  vertInput = Input.GetAxis("Vertical_" + playerNum.ToString());
+
+		transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime * horizInput);
+		transform.localPosition += (transform.forward * walkSpeed * vertInput * Time.deltaTime);
+	}
+}
