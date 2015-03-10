8ca81ae66a79128b52a5f5c70ebee1127679194c
Billy Wolfington <man_of_magik903@yahoo.com>
2015-03-09 20:21:57 -0400

    Added a new public member function to GameManager, started ShopMenu class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -112,4 +112,8 @@ public class GameManager : MonoBehaviour {
 			updateTeamText(baseId);
 		}
 	}
+
+	public ResourceCount GetTeamResourceInfo(int team_id) {
+		return teamResources[team_id];
+	}
 }
---
+++ Assets/Scripts/ShopMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+struct Item {
+	public int wood_cost;
+	public int stone_cost;
+	public string name;
+}
+
+public class ShopMenu : MonoBehaviour {
+	
+	GameManager manager = null;
+	List<Item> items;
+	
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("GameManager").GetComponent<GameManager>();
+		items = new List<Item>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	bool MakePurchase(int team_id, int item_id) {
+		ResourceCount team_count = manager.GetTeamResourceInfo(team_id);
+		Item item = items[item_id];
+		if (CanPurchase(item, team_count)) {
+			manager.RemoveResources(team_id, ResourceType.stone, item.stone_cost);
+			manager.RemoveResources(team_id, ResourceType.wood, item.wood_cost);
+			return true;
+		} 
+		return false;
+	}
+	
+	bool CanPurchase(Item item, ResourceCount resource_count) {
+		return item.wood_cost <= resource_count.wood && item.stone_cost <= resource_count.stone;
+	}
+}
