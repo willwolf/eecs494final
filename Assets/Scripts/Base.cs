ed90c0e5190ed01fa77aa9615d22948ed5968634
Tim W <twurman@umich.edu>
2015-03-11 13:37:47 -0400

    updated wall code

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -5,30 +5,30 @@ using System.Collections;
 public class Base : MonoBehaviour {
 
 	GameManager gm;
-	public bool hasWalls;
 
 	void Start() {
 		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-	}
-
-	void Update(){
-		if(!hasWalls){
-			foreach(Transform child in GetComponentsInChildren<Transform>()){
-				if(child.tag == "Wall"){
-					child.renderer.enabled = false;
-					child.collider.enabled = false;
-				}
-			}
-		} else {
-			foreach(Transform child in GetComponentsInChildren<Transform>()){
-				if(child.tag == "Wall"){
-					child.renderer.enabled = true;
-					child.collider.enabled = true;
-				}
+		foreach(Transform child in GetComponentsInChildren<Transform>()){
+			if(child.tag == "Wall"){
+				child.renderer.enabled = false;
+				child.collider.enabled = false;
 			}
 		}
 	}
+	
+	void Update(){
 
+	}
+
+	public void TurnOnWalls(){
+		foreach(Transform child in GetComponentsInChildren<Transform>()){
+			if(child.tag == "Wall"){
+				child.renderer.enabled = true;
+				child.collider.enabled = true;
+			}
+		}
+	}
+	
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
 			return;
