748b068bb3bc8be788f501e73fb166bf5b27af9a
Billy Wolfington <man_of_magik903@yahoo.com>
2015-03-25 14:26:47 -0400

    Prevent rocks from rolling forever

--- Assets/Scripts/ScatteredObj.cs
+++ Assets/Scripts/ScatteredObj.cs
@@ -5,10 +5,14 @@ public class ScatteredObj : MonoBehaviour {
 	public ResourceType type = ResourceType.none;
 	public float canPickUpAt = 0f;
 	public float disableTime = 0.5f;
+	public float stopRollingAt = 0f;
+	public float rollTime = 2f;
+	public float speedSlow = 0.5f;
 
 	// Use this for initialization
 	void Start () {
 		canPickUpAt = Time.time + disableTime;
+		stopRollingAt = Time.time + rollTime;
 	}
 	
 	// Update is called once per frame
@@ -16,9 +20,18 @@ public class ScatteredObj : MonoBehaviour {
 		if (type == ResourceType.none) {
 			throw new UnassignedReferenceException("ScatteredObj script's type is none");
 		}
+		if (SlowRoll()) {
+			Vector3 decrement = rigidbody.velocity;
+			decrement *= speedSlow;
+			rigidbody.velocity -= decrement;
+		}
 	}
 
 	public bool CanPickUp() {
 		return Time.time >= canPickUpAt;
+	}
+
+	public bool SlowRoll() {
+		return Time.time >= stopRollingAt;
 	}
 }
