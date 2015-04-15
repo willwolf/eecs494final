d86b88edadaed5e0767ed21e102a1b8dd93a597b
Billy Wolfington <man_of_magik903@yahoo.com>
2015-04-15 14:57:19 -0400

    Fixed issue of holding trap/box and useing TakeAction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -614,8 +614,8 @@ public class PlayerController : MonoBehaviour {
             Destroy(t.gameObject);
         }
         // Remove trap so that resource box is dropped
-		DropResourceBox();
 		ScatterResources();
+		DropResourceBox();
 
 		foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>()) {
 			renderer.enabled = false;
@@ -755,7 +755,10 @@ public class PlayerController : MonoBehaviour {
 		RaycastHit hitinfo;
         ResourceBox rbox = GetComponentInChildren<ResourceBox>();
         Trap trap = GetComponentInChildren<Trap>();
-		if (IsInRange(out hitinfo, "Resource") && !shopOpen && (!trap || !rbox)) {
+		if (trap || rbox) {
+			return;
+		}
+		if (IsInRange(out hitinfo, "Resource") && !shopOpen) {
 			Resource r = hitinfo.transform.GetComponent<Resource>();
 			if (r == null) {
 				throw new UnassignedReferenceException("Resource layer object does not have Resource script attached");
@@ -773,10 +776,12 @@ public class PlayerController : MonoBehaviour {
 			if (drop.playerBaseGO.GetInstanceID() != homeBase_GO.GetInstanceID()) {
 				StealResource(drop);
 			}
-		} else if(IsInRange(out hitinfo, "ResourceBox") && !shopOpen && !rbox && !trap){
+		} else if(IsInRange(out hitinfo, "ResourceBox") && !shopOpen){
 			//pick up resource box
-			hitinfo.transform.position = this.transform.position + this.transform.up * 0.5f + this.transform.forward;
-			hitinfo.transform.SetParent(this.transform);
+			if (hitinfo.transform.gameObject.layer != LayerMask.NameToLayer("Player")) {
+				hitinfo.transform.position = this.transform.position + this.transform.up * 0.5f + this.transform.forward;
+				hitinfo.transform.SetParent(this.transform);
+			}
 		}
 	}
 
