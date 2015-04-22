390e48c377c52fa28d700e5f1c6e22ce75259b0e
Tim W <twurman@umich.edu>
2015-04-22 01:35:46 -0400

    added main menu screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public Text playText;
+	float flashTimer;
+
+	// Use this for initialization
+	void Start () {
+		flashTimer = Time.time + 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > flashTimer){
+			playText.enabled = !playText.enabled;
+			flashTimer = Time.time + 1;
+		}
+
+		if(Input.GetKeyDown(KeyCode.P)){
+			Application.LoadLevel(0);
+		}
+	}
+}
